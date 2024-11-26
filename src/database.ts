62842c17bc369d734fcf3efea84f0cebddbf9a32
leonardovitoecker <leonardovitoecker@gmail.com>
2024-11-26 20:33:26 -0300

    build

--- src/database.ts
+++ src/database.ts
@@ -7,10 +7,13 @@ if (!process.env.DATABASE_URL) {
   throw new Error("No DATABASE_URL environment variable found.");
 }
 export const config: Knex.Config = {
-  client: "sqlite3",
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === "sqlite"
+      ? {
+          filename: env.DATABASE_URL,
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
